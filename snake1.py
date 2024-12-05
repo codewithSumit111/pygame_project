d8fda27009a46575ed0e0cb7d3616b34b7626c95
Sumit <sumitdarya6217@gmail.com>
2024-12-05 20:20:32 +0530

    Score on GameWindow/ Screen

--- snake1.py
+++ snake1.py
@@ -25,12 +25,18 @@ velocity_x = 0
 velocity_y = 0
 food_x = random.randint(20, int(screen_width/2))
 food_y = random.randint(20, int(screen_height/2))
-snake_size = 10
+snake_size = 15
+init_velocity = 5
 fps = 30
 score = 0
 
-
 clock = pygame.time.Clock()
+
+font = pygame.font.SysFont(None, 40)
+def text_screen(text, color, x, y):
+    screen_text = font.render(text, True, color)
+    gameWindow.blit(screen_text, [x,y])
+
 #Game loop
 while not exit_game:
     for event in pygame.event.get():
@@ -38,27 +44,27 @@ while not exit_game:
             exit_game == True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                velocity_x = 7
+                velocity_x = init_velocity
                 velocity_y = 0
             if event.key == pygame.K_LEFT:
-                velocity_x = -7
+                velocity_x = -init_velocity
                 velocity_y = 0
             if event.key == pygame.K_UP:
-                velocity_y = -7
+                velocity_y = -init_velocity
                 velocity_x = 0
             if event.key == pygame.K_DOWN:
-                velocity_y = 7
+                velocity_y = init_velocity
                 velocity_x = 0
 
     snake_x += velocity_x
     snake_y += velocity_y
 
-    if abs(snake_x - food_x)<5 and abs(snake_y - food_y)<5:
+    if abs(snake_x - food_x)<7 and abs(snake_y - food_y)<7:
         score += 1
-        print("SCORE: ",score)
         food_x = random.randint(20, int(screen_width/2))
         food_y = random.randint(20, int(screen_height/2))
     gameWindow.fill(white)
+    text_screen("SCORE: "+ str(score*10), red, 5, 5)
     pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
     pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
